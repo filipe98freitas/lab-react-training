caf2d146f649ba20e6ac236d3bc6b4ff2485aae2
filipe98freitas <filipe98freitas@gmail.com>
2021-08-31 21:51:45 +0100

    Create greeting.js

---
+++ src/components/greeting.js
@@ -0,0 +1,20 @@
+function Greetings(props) {
+  function greeting() {
+    if (props.lang === 'de') {
+      return <p>Hallo</p>;
+    } else if (props.lang === 'en') {
+      return <p>Hello</p>;
+    } else if (props.lang === 'es') {
+      return <p>Hola</p>;
+    } else if (props.lang === 'fr') {
+      return <p>Bonjour</p>;
+    }
+  }
+  return (
+    <div>
+      {' '}
+      {greeting()} {props.children}{' '}
+    </div>
+  );
+}
+export default Greetings;
