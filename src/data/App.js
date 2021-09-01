6bad61c59a9cca9718a8a837b352f1eba5613775
filipe98freitas <filipe98freitas@gmail.com>
2021-09-01 21:42:21 +0100

    Update App.js

--- src/data/App.js
+++ src/data/App.js
@@ -32,6 +32,13 @@ function App() {
       <h1>BoxColor</h1>
       <BoxColor r={255} g={0} b={0} />
       <BoxColor r={128} g={255} b={0} />
+      <h1>Rating</h1>
+      <Rating>0</Rating>
+      <Rating>1.49</Rating>
+      <Rating>1.5</Rating>
+      <Rating>3</Rating>
+      <Rating>4</Rating>
+      <Rating>5</Rating>
     </div>
   );
 }
